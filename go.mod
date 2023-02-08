1d4246b67b9f7a5b70848ad7b6d9f5c72effef9b
Louis Hollingworth <louis@hollingworth.ch>
2023-02-08 19:36:03 +0000

    Bot now starts but doesn't do anything.
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/lucxjo/klara
 
 go 1.19
+
+require (
+	github.com/disgoorg/disgo v0.15.0
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/disgoorg/json v1.0.0 // indirect
+	github.com/disgoorg/log v1.2.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/sasha-s/go-csync v0.0.0-20210812194225-61421b77c44b // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/exp v0.0.0-20220325121720-054d8573a5d8 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-    fmt.Printf("Saluton, mondo!")
+	log := logrus.New()
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("An error occurred when getting environment variables: %v\n", err)
+	}
+
+	if os.Getenv("DISCORD_TOKEN") == "" {
+		log.Fatalf("Missing DISCORD_TOKEN environment variable\n")
+	}
+	client, err := disgo.New(os.Getenv("DISCORD_TOKEN"),
+		bot.WithGatewayConfigOpts(
+			gateway.WithIntents(
+				gateway.IntentGuilds,
+				gateway.IntentGuildMessages,
+				gateway.IntentDirectMessages,
+			),
+		),
+		bot.WithEventListenerFunc(func(e *events.Ready) {
+			log.Infof("The bot is now ready!\n")
+		}),
+	)
+	if err != nil {
+		log.Fatalf("Error creating Discord client: %v\n", err)
+	}
+
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		log.Fatalf("An error occurred when opening the client: %v\n", err)
+	}
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+	<-s
 }
